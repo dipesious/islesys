6f9c2d54cca51c933b0530e156eedeec2ecd618c
dipesious <dipeshbhoir@hotmail.com>
2022-11-17 15:03:26 +0530

    update

--- src/app/components/who-we-are/who-we-are.component.ts
+++ src/app/components/who-we-are/who-we-are.component.ts
@@ -8,6 +8,21 @@ import { Component, OnInit } from '@angular/core';
 export class WhoWeAreComponent implements OnInit {
 
   loaded = false;
+  brands = [
+    {name:"icons", img:"/brand-icons.jpeg", link:""},
+    {name:"maps", img:"/brand-maps.jpeg", link:""},
+    {name:"datasets", img:"/brand-datasets.jpeg", link:""},
+    {name:"palettes", img:"/brand-palettes.jpeg", link:""},
+    {name:"gradients", img:"/brand-gradients.jpeg", link:""},
+    {name:"patterns", img:"/brand-patterns.jpeg", link:""},
+    {name:"fonts", img:"/brand-fonts.jpeg", link:""},
+    {name:"shades", img:"/brand-shades.jpeg", link:""},
+    {name:"aimtera", img:"/brand-aimtera.jpeg", link:""},
+    {name:"frycold", img:"/brand-frycold.jpeg", link:""},
+    {name:"mapwale", img:"/brand-xxx.jpeg", link:""},
+    {name:"iconwho", img:"/brand-xxx.jpeg", link:""},
+    // {name:"xxx", img:"/brand-xxx.jpeg", link:""},
+  ]
 
   constructor() { 
     this.execute()
